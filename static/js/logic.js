5766a2d730594f4bed3b0165b0631139da94d050
Theodora Nutaitis-Lopez <99051640+TheodoraNell@users.noreply.github.com>
2022-09-14 17:30:39 -0700

    add script for map legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,7 +14,7 @@ let map = L.map("mapid", {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: "pk.eyJ1IjoidGhlb2RvcmFsb3BleiIsImEiOiJjbDd2ZDFldWwwNTR2M3psZXFnNDhoYTRhIn0.-ZN14KAiOoQhz5YixysM7w"
+    accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
@@ -59,6 +59,25 @@ function getColor(le) {
   return "#ea2c2c";
 }
 
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [36, 60, 65, 70, 75, 80],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
 
 // Loop through the cities array and create one marker for each country.
 countryData.forEach(function(country) {
@@ -67,3 +86,5 @@ countryData.forEach(function(country) {
   .bindPopup("<h2>" + country.country + "</h2> <hr> <h3> Life Expectancy:  " + country.life_expectancy + "</h3> <hr> <h3> Drinking Water(%):  " + country.drinking_water + "</h3> <hr> <h3> Hand Washing(%):  " + country.hand_washing + "</h3> <hr> <h3> Safe Sanitation(%):  " + country.safe_sanitation + "</h3>")
   .addTo(map);
 });
+
+
