c7972902250f246ed2ac0dec10a9eec4eab965d9
Theodora Nutaitis-Lopez <99051640+TheodoraNell@users.noreply.github.com>
2022-09-14 17:40:15 -0700

    update site-specific key

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,7 +14,7 @@ let map = L.map("mapid", {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoidGhlb2RvcmFsb3BleiIsImEiOiJjbDd2ZDFldWwwNTR2M3psZXFnNDhoYTRhIn0.-ZN14KAiOoQhz5YixysM7w"
 });
 
 // Then we add our 'graymap' tile layer to the map.
